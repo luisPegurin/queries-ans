7690d591ad2d0c6d56139e45a81c44c21f9454d6
luisPegurin <luis.hp.fut@gmail.com>
2021-12-10 20:45:17 -0300

    add table 'operadoras'

--- load.sql
+++ load.sql
@@ -1,5 +1,32 @@
 DROP TABLE IF EXISTS demonstracoes_contabeis;
 DROP TABLE IF EXISTS tmp;
+DROP TABLE IF EXISTS operadoras;
+
+
+CREATE TABLE operadoras (
+    registro_ans int primary key,
+    cnpj varchar(20),
+    razao_social varchar(200),
+    nome_fantasia varchar(100),
+    modalidade varchar(100),
+    logradouro varchar(200),
+    numero varchar(50),
+    complemento varchar(50),
+    bairro varchar(50),
+    cidade varchar(50),
+    uf char(2),
+    cep varchar(10),
+    ddd char(2),
+    telefone varchar(20),
+    fax varchar(20),
+    email varchar(50),
+    representante varchar(50),
+    cargo_representante varchar(50),
+    regiao_de_comercializacao varchar(5),
+    Data_Registro_ANS date
+);
+
+\copy operadoras from './Relatorio_cadop.csv' WITH (FORMAT CSV, HEADER, ENCODING windows1252, DELIMITER ';')
 
 CREATE TABLE demonstracoes_contabeis (
 	INDEX serial primary key,
@@ -30,6 +57,5 @@ INSERT INTO  demonstracoes_contabeis  (data,REG_ANS,CD_CONTA_CONTABIL,DESCRICAO,
 	(SELECT
 		data,REG_ANS,CD_CONTA_CONTABIL,DESCRICAO, cast(replace(VL_SALDO_FINAL,',','.') AS numeric) 
 		FROM tmp);
-
 
 DROP TABLE tmp;	
--- queries.sql
+++ queries.sql
@@ -1,14 +1,21 @@
- SELECT reg_ans, SUM(VL_SALDO_FINAL) FROM demonstracoes_contabeis 
- 	WHERE descricao = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR '
- 		AND data >= '2021-07-01'
- 	GROUP BY reg_ans
- 	ORDER BY SUM(VL_SALDO_FINAL) DESC
- 	LIMIT 10;
+-- 10 operadoras que mais tiveram despesas no último trimestre
+select o.razao_social, d.reg_ans, sum(d.VL_SALDO_FINAL)
+	from demonstracoes_contabeis d
+	inner join operadoras o on d.reg_ans = o.registro_ans
+	where d.descricao = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR '
+	    and d.data >= '2021-07-01'
+	group by d.reg_ans, o.razao_social
+	order by sum(d.VL_SALDO_FINAL) DESC
+	limit 10;
 
+-- 10 operadoras que mais tiveram despesas no último ano
+select o.razao_social, d.reg_ans, sum(d.VL_SALDO_FINAL)
+	from demonstracoes_contabeis d
+	inner join operadoras o on d.reg_ans = o.registro_ans
+	where d.descricao = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR '
+	    and d.data >= '2020-07-01'
+	group by d.reg_ans, o.razao_social
+	order by sum(d.VL_SALDO_FINAL) DESC
+	limit 10;
 
- SELECT reg_ans, SUM(VL_SALDO_FINAL) FROM demonstracoes_contabeis 
- 	WHERE descricao = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR '
- 		AND data >= '2020-07-01'
- 	GROUP BY reg_ans
- 	ORDER BY SUM(VL_SALDO_FINAL) DESC
- 	LIMIT 10;
+ 
